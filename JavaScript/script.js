331732f8d8ce80c6f7f58cc682cc50235b626b56
HushWasp28 <70871175+HushWasp28@users.noreply.github.com>
2021-03-13 23:36:22 +0000

    Add files via upload

---
+++ JavaScript/script.js
@@ -0,0 +1,10 @@
+"use strict"
+var name = prompt("Your Name");
+var password = "Desmond";
+prompt("Enter the password")
+if (password == "Desmond") {
+	alert("Welcome"+name);
+}
+else {
+	window.close();
+}
