e537d49d3528fbaa1f89b6767827feaded0a1254
HushWasp28 <70871175+HushWasp28@users.noreply.github.com>
2021-03-14 08:02:31 +0000

    Update script.js

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,10 +1,11 @@
 "use strict"
 var name = prompt("Your Name");
-var password = "Desmond";
+var password = "HVMS";
 prompt("Enter the password")
 if (password == "Desmond") {
 	alert("Welcome"+name);
 }
 else {
+	alert("Wrong password");
 	window.close();
 }
